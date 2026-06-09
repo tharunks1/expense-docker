f7ebbda6e8b183aac9edb4f766784fb43ad140d9
SRINIVAS_THARUN <88927252+tharunks1@users.noreply.github.com>
2026-06-09 09:40:45 +0530

    Update TransactionService.js

--- backend/TransactionService.js
+++ backend/TransactionService.js
@@ -2,7 +2,7 @@ const dbcreds = require('./DbConfig');
 const mysql = require('mysql2'); // Change to mysql2
 
 const con = mysql.createConnection({
-    host: process.env.DB_HOST || dbcreds.DB_HOST,
+    host: process.env.DB_HOST || dbcreds.DB_HOST, 
     user: process.env.DB_USER || dbcreds.DB_USER,
     password: process.env.DB_PWD || dbcreds.DB_PWD,
     database: process.env.DB_DATABASE || dbcreds.DB_DATABASE
